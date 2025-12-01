c9ab0d165938e6ad05be3af69e8fae970be9a762
Горбунова Нина <gnina56565@gmail.com>
2025-12-01 10:12:57 +0400

    initial coomit mp-18

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int myAge = 19;
+        String myName = "Nina";
+        System.out.println("Меня зовут " + myName + " мне " +  myAge);
+    }
+}
